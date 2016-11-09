ef9cb4f7832842461adb26dce231f151053eb44c
Denis <denis.rakhuba@gmail.com>
2016-11-09 14:10:32 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,3 @@
-//Problem: User interaction doesn't provide desired results.
-//Solution: Add interactivty so the user can manage daily tasks.
-
 var taskInput = document.getElementById("new-task"); //new-task
 var addButton = document.getElementsByTagName("button")[0]; //first button
 var incompleteTasksHolder = document.getElementById("incomplete-tasks"); //incomplete-tasks
